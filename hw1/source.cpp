54ae0de192030aec61b3e9b6a5de501dd889f216
hyoputer <ajtwlsalsdl0@naver.com>
2017-10-11 17:52:32 +0900

    1 finish

--- hw1/source.cpp
+++ hw1/source.cpp
@@ -19,13 +19,11 @@ int main ()
         stack<char> cal;
         stack<pi> reg;
         queue<char> Queue;
-        map<pair<int, char>, vector<int> > m;
+        map<pair<int, char>, vector<int> > m; //nfa table '(state, alphabet)-> vector of dest state'
         char rexp[5], exp[100], file[100];
-        int sidx = 0;
-        int visited[100] = {};
+        int sidx = 0; // new state number
         fscanf(in, "%s %s %s", rexp, exp, file);
-        //printf("%s %s %s", rexp, exp, file);
-        for (int i = 0; exp[i] != 0; i++)
+        for (int i = 0; exp[i] != 0; i++) //infix regex to postfix in queue
         {
             char cur = exp[i];
             if (cur == '0' || cur == '1')
@@ -38,7 +36,7 @@ int main ()
             else if (cur != '(')
                 cal.push(cur);
         }
-        while (!Queue.empty())
+        while (!Queue.empty()) // make nfa table from postfix regex
         {
             char front = Queue.front();
             if (front == '0' || front == '1')
@@ -80,8 +78,70 @@ int main ()
                 m[make_pair(f.second, 'e')].push_back(b.first);
                 reg.push(make_pair(f.first, b.second));
             }
-            //printf("%c", Queue.front());
             Queue.pop();
         }
+        pi p = reg.top();
+        int end = p.second;
+        queue<int> res;
+        queue<int> start;
+        res.push(p.first);
+        bool f = true;
+        for (int i = 0; file[i] != 0; i++) //using BFS to run NFA
+        {
+            queue<int> empty;
+            swap(res, start);
+            swap(res, empty);
+            char cur = file[i];
+            int visited[100] = {};
+            if (start.empty())
+            {
+                f = false;
+                break;
+            }
+            while(!start.empty())
+            {
+                int cs = start.front();
+                visited[cs] = 1;
+                start.pop();
+                vector<int> v = m[make_pair(cs, 'e')];
+                for (int j = 0; j < v.size(); j++)
+                {
+                    if (visited[v[j]] != 0)
+                        continue;
+                    start.push(v[j]);
+                }
+                if (!m[make_pair(cs, cur)].empty())
+                    res.push(m[make_pair(cs, cur)][0]);
+            }
+        }
+        if (f)
+        {
+            int visited[100] = {};
+            bool ff = false;
+            while(!res.empty())
+            {
+                int cs = res.front();
+                visited[cs] = 1;
+                if (end == cs)
+                {
+                    ff = true;
+                    break;
+                }
+                vector<int> v = m[make_pair(cs, 'e')];
+                for (int j = 0; j < v.size(); j++)
+                {
+                    if (visited[v[j]] != 0)
+                        continue;
+                    res.push(v[j]);
+                }
+                res.pop();
+            }
+            if (!ff)
+                f = false;
+        }
+        if (f)
+            fprintf(out, "yes\n");
+        else
+            fprintf(out, "no\n");
     }
 }
